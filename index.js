157102a134804252b37c88a628796f2d31884c0c
delet-this <33498862+delet-this@users.noreply.github.com>
2023-01-31 21:01:16 +0200

    Solve 3.18

--- index.js
+++ index.js
@@ -52,23 +52,31 @@ let persons = [
     },
 ]
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>` +
-        `<p>${Date()}</p>`
-    )
+app.get('/api/info', (request, response, next) => {
+    Person.countDocuments({})
+        .then(count => {
+            response.send(
+                `<p>Phonebook has info for ${count} people</p>` +
+                `<p>${Date()}</p>`
+            )
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -82,7 +90,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -108,9 +116,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
